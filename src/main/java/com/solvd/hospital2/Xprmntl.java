ef2a0b267ae11f22f5c35b5a7791216325c1879c
Anton Karavikou <antontenix@gmail.com>
2021-10-22 19:25:30 +0300

    New class was added

---
+++ src/main/java/com/solvd/hospital2/Xprmntl.java
@@ -0,0 +1,23 @@
+package com.solvd.hospital2;
+
+public class Xprmntl {
+
+    private int xprmntl;
+    private String type;
+
+    public int getXprmntl() {
+        return xprmntl;
+    }
+
+    public void setXprmntl(int xprmntl) {
+        this.xprmntl = xprmntl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
