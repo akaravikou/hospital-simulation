23ce94ea96a1ef4e0b5aaaaf69f88aed8ee4325f
Anton Karavikou <antontenix@gmail.com>
2021-10-22 19:05:56 +0300

    README was modified

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hospital-simulation
 
+A program describing a hospital that
+also contains a service class that determines whether the patient is healthy or not.
---
+++ src/main/java/com/solvd/hospital2/Xprmnt.java
@@ -0,0 +1,14 @@
+package com.solvd.hospital2;
+
+public class Xprmnt {
+
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
